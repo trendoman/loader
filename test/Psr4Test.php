ac4fcb0c0be34fe25cbde2308d6fa0523977f9ef
Cyril <consultant@seeren.fr>
2017-05-30 20:24:38 +0200

    Add coverage notation

--- test/Psr4Test.php
+++ test/Psr4Test.php
@@ -10,12 +10,16 @@
  *
  * @copyright (c) Cyril Ichti <consultant@seeren.fr>
  * @link http://www.seeren.fr/ Seeren
- * @version 1.0.3
+ * @version 1.1.1
  */
 
 namespace Seeren\Loader\Test;
 
-use Seeren\Loader\{LoaderInterface, Psr4, ClassMap};
+use Seeren\Loader\LoaderInterface;
+use Seeren\Loader\Psr4;
+use Foo\Bar\Psr4Foo;
+use Foo\Bar\Psr4Bar;
+
 use ReflectionClass;
 
 /**
@@ -26,7 +30,7 @@ use ReflectionClass;
  * @subpackage Test
  * @final
  */
-final class Psr4Test extends LoaderInterfaceTest
+class Psr4Test extends AbstractLoaderTest
 {
 
    /**
@@ -40,15 +44,96 @@ final class Psr4Test extends LoaderInterfaceTest
    }
 
    /**
-    * Test Psr4::fileName
+    * @covers \Seeren\Loader\Loader::__construct
+    * @covers \Seeren\Loader\Psr4::__construct
+    * @covers \Seeren\Loader\Psr4::load
+    * @covers \Seeren\Loader\Psr4::fileName
     */
-   public final function testFileName()
+   public function testLoadFalse()
+   {
+       parent::testLoadFalse();
+   }
+
+   /**
+    * @covers \Seeren\Loader\Loader::__construct
+    * @covers \Seeren\Loader\Psr4::__construct
+    * @covers \Seeren\Loader\Psr4::register
+    */
+   public function testRegister()
+   {
+       parent::assertRegister();
+   }
+
+   /**
+    * @covers \Seeren\Loader\Loader::__construct
+    * @covers \Seeren\Loader\Psr4::__construct
+    * @covers \Seeren\Loader\Psr4::register
+    * @covers \Seeren\Loader\Psr4::unregister
+    */
+   public function testUnregister()
+   {
+       parent::assertUnregister();
+   }
+
+   /**
+    * @covers \Seeren\Loader\Loader::__construct
+    * @covers \Seeren\Loader\Psr4::__construct
+    * @covers \Seeren\Loader\Psr4::addPrefix
+    * @covers \Seeren\Loader\Psr4::load
+    * @covers \Seeren\Loader\Psr4::fileName
+    * @covers \Seeren\Loader\Psr4::find
+    * @covers \Seeren\Loader\Psr4::parseBaseDirectory
+    * @covers \Seeren\Loader\Psr4::parseFileName
+    * @covers \Seeren\Loader\Psr4::parsePrefix
+    */
+   public function testAddPrefix()
    {
        $loader = $this->getLoader();
        $loader->__construct(__DIR__);
-       $loader->addPrefix("Seeren\\Loader\\", __DIR__ . "/../src");
-       $this->assertTrue($loader->load(ClassMap::class)
-                      || class_exists(ClassMap::class));
+       $loader->addPrefix("Foo\\Bar\\", __DIR__ . "/mock");
+       $this->assertTrue($loader->load(Psr4Foo::class));
+   }
+
+   /**
+    * @covers \Seeren\Loader\Loader::__construct
+    * @covers \Seeren\Loader\Psr4::__construct
+    * @covers \Seeren\Loader\Psr4::addPrefix
+    * @covers \Seeren\Loader\Psr4::load
+    * @covers \Seeren\Loader\Psr4::fileName
+    * @covers \Seeren\Loader\Psr4::find
+    * @covers \Seeren\Loader\Psr4::parseBaseDirectory
+    * @covers \Seeren\Loader\Psr4::parseFileName
+    * @covers \Seeren\Loader\Psr4::parsePrefix
+    */
+   public function testAddPrefixWithMultiBaseDirectory()
+   {
+       $loader = $this->getLoader();
+       $loader->__construct(__DIR__);
+       $loader->addPrefix("Foo\\Bar\\", [
+           __DIR__ . "/fake",
+           __DIR__ . "/mock",
+           
+       ]);
+       $this->assertTrue($loader->load(Psr4Bar::class));
+   }
+
+   /**
+    * @covers \Seeren\Loader\Loader::__construct
+    * @covers \Seeren\Loader\Psr4::__construct
+    * @covers \Seeren\Loader\Psr4::addPrefix
+    * @covers \Seeren\Loader\Psr4::parsePrefix
+    * @covers \Seeren\Loader\Psr4::removePrefix
+    * @covers \Seeren\Loader\Psr4::load
+    * @covers \Seeren\Loader\Psr4::parseBaseDirectory
+    * @covers \Seeren\Loader\Psr4::fileName
+    */
+   public function testRemovePrefix()
+   {
+       $loader = $this->getLoader();
+       $loader->__construct(__DIR__);
+       $loader->addPrefix("Foo\\Bar\\", __DIR__ . "/mock");
+       $loader->removePrefix("Foo\\Bar\\");
+       $this->assertFalse($loader->load(Psr4Foo::class));
    }
 
 }
