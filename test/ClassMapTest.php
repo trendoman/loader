fa53df8f8b972661d2a4e8ce8339d769533bd879
Cyril <consultant@seeren.fr>
2017-05-30 20:42:59 +0200

    Test addClass and removeClass

--- test/ClassMapTest.php
+++ test/ClassMapTest.php
@@ -10,17 +10,14 @@
  *
  * @copyright (c) Cyril Ichti <consultant@seeren.fr>
  * @link http://www.seeren.fr/ Seeren
- * @version 1.0.1
+ * @version 1.1.1
  */
 
 namespace Seeren\Loader\Test;
 
 use Seeren\Loader\LoaderInterface;
-use Seeren\Loader\Psr4;
-use Foo\Bar\Psr4Foo;
-use Foo\Bar\Psr4Bar;
-use ReflectionClass;
 use Seeren\Loader\ClassMap;
+use ReflectionClass;
 
 /**
  * Class for test ClassMap
@@ -74,65 +71,20 @@ class ClassMapTest extends AbstractLoaderTest
        parent::assertUnregister();
    }
 
-//    /**
-//     * @covers \Seeren\Loader\Loader::__construct
-//     * @covers \Seeren\Loader\Psr4::__construct
-//     * @covers \Seeren\Loader\Psr4::addPrefix
-//     * @covers \Seeren\Loader\Psr4::load
-//     * @covers \Seeren\Loader\Psr4::fileName
-//     * @covers \Seeren\Loader\Psr4::find
-//     * @covers \Seeren\Loader\Psr4::parseBaseDirectory
-//     * @covers \Seeren\Loader\Psr4::parseFileName
-//     * @covers \Seeren\Loader\Psr4::parsePrefix
-//     */
-//    public function testAddPrefix()
-//    {
-//        $loader = $this->getLoader();
-//        $loader->__construct(__DIR__);
-//        $loader->addPrefix("Foo\\Bar\\", __DIR__ . "/mock");
-//        $this->assertTrue($loader->load(Psr4Foo::class));
-//    }
-
-//    /**
-//     * @covers \Seeren\Loader\Loader::__construct
-//     * @covers \Seeren\Loader\Psr4::__construct
-//     * @covers \Seeren\Loader\Psr4::addPrefix
-//     * @covers \Seeren\Loader\Psr4::load
-//     * @covers \Seeren\Loader\Psr4::fileName
-//     * @covers \Seeren\Loader\Psr4::find
-//     * @covers \Seeren\Loader\Psr4::parseBaseDirectory
-//     * @covers \Seeren\Loader\Psr4::parseFileName
-//     * @covers \Seeren\Loader\Psr4::parsePrefix
-//     */
-//    public function testAddPrefixWithMultiBaseDirectory()
-//    {
-//        $loader = $this->getLoader();
-//        $loader->__construct(__DIR__);
-//        $loader->addPrefix("Foo\\Bar\\", [
-//            __DIR__ . "/fake",
-//            __DIR__ . "/mock",
-           
-//        ]);
-//        $this->assertTrue($loader->load(Psr4Bar::class));
-//    }
-
-//    /**
-//     * @covers \Seeren\Loader\Loader::__construct
-//     * @covers \Seeren\Loader\Psr4::__construct
-//     * @covers \Seeren\Loader\Psr4::addPrefix
-//     * @covers \Seeren\Loader\Psr4::parsePrefix
-//     * @covers \Seeren\Loader\Psr4::removePrefix
-//     * @covers \Seeren\Loader\Psr4::load
-//     * @covers \Seeren\Loader\Psr4::parseBaseDirectory
-//     * @covers \Seeren\Loader\Psr4::fileName
-//     */
-//    public function testRemovePrefix()
-//    {
-//        $loader = $this->getLoader();
-//        $loader->__construct(__DIR__);
-//        $loader->addPrefix("Foo\\Bar\\", __DIR__ . "/mock");
-//        $loader->removePrefix("Foo\\Bar\\");
-//        $this->assertFalse($loader->load(Psr4Foo::class));
-//    }
+   /**
+    * @covers \Seeren\Loader\Loader::__construct
+    * @covers \Seeren\Loader\ClassMap::__construct
+    * @covers \Seeren\Loader\ClassMap::addClass
+    * @covers \Seeren\Loader\ClassMap::load
+    * @covers \Seeren\Loader\ClassMap::fileName
+    * @covers \Seeren\Loader\ClassMap::removeClass
+    */
+   public function testAddAndRemoveClass()
+   {
+       $loader = $this->getLoader();
+       $loader->__construct(__DIR__);
+       $loader->addClass("ClassMapFoo", __DIR__ . "/mock/ClassMapFoo.php");
+       $this->assertTrue($loader->load("ClassMapFoo"));
+   }
 
 }
